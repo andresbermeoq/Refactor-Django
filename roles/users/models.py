3ef84a8066c7500f34a9b216016120152cffab81
andresbermeoq <andresbermeoq1@gmail.com>
2024-05-30 11:52:24 -0500

    feat: :construction: Add models
    Add models and create the apps with the definition of roles.

--- roles/users/models.py
+++ roles/users/models.py
@@ -1,11 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=3, unique=True)
 
 class Role(models.Model):
-    pass
+    name = models.CharField(max_length=100)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    users = models.ManyToManyField(User, related_name='roles', blank=True)
 
 class Area(models.Model):
-    pass
+    name = models.CharField(max_length=100)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+
+class RoleArea(models.Model):
+    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+    area = models.ForeignKey(Area, on_delete=models.CASCADE)
+    is_leader = models.BooleanField(default=True)
 
 class Permission(models.Model):
-    pass
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=50, unique=True)
+
+class RolePermission(models.Model):
+    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+    permission = models.ForeignKey(Permission, on_delete=models.CASCADE)
